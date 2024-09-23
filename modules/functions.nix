c0315879162b8b24ea68236d22b1229cda92c498
Manas Sambare <manas.sambare@gmail.com>
2024-09-23 21:38:54 +0530

    [Functions] Add conditionalImports

--- modules/functions.nix
+++ modules/functions.nix
@@ -1,29 +1,37 @@
 { pkgs, lib, ... }:
 
 let
-  makeModuleConfig = { options, current, module_name }:
+  makeModuleConfig = { options, current, module_name, conditional_imports ? [] }:
     let
       currentList = if builtins.isList current && current != []
         then current
         else if builtins.isString current
              then [ current ]
         else [];
+
       invalid_options = builtins.filter (t: !(builtins.elem t options)) currentList;
+
     in
     assert lib.assertMsg (invalid_options == [])
       "Error: Invalid options specified: ${toString invalid_options}";
-    {
-      imports = lib.lists.flatten (map (option:
-        if builtins.elem option options
-        then [ (import ./${module_name}/${option}) ] # Fixing the syntax here for importing
-        else []
-      ) currentList);
 
-      config = lib.mkMerge (map (option:
-        lib.mkIf (builtins.elem option options) {
-          "${option}".enable = true; # Ensure correct string interpolation here
-        }
-      ) currentList);
+    {
+      imports = lib.lists.flatten (
+        # Existing import logic for each window manager option
+        map (option:
+          if builtins.elem option options
+          then [ (import ./${module_name}/${option}) ]
+          else []
+        ) currentList
+      ) ++ conditional_imports; # Add the conditional imports at the end
+
+      config = lib.mkMerge (
+        map (option:
+          lib.mkIf (builtins.elem option options) {
+            "${option}".enable = true;
+          }
+        ) currentList
+      );
     };
 in
 {
--- modules/window_manager/default.nix
+++ modules/window_manager/default.nix
@@ -2,13 +2,22 @@
 let
   functions = import ../functions.nix { inherit pkgs lib; };
   window_manager_options = [ "i3" ];
+
+  # Define imports conditionally based on window_manager
+  conditional_imports = if builtins.isList window_manager then
+    if builtins.elem "hyprland" window_manager
+    then []
+    else [ ./x11.nix ]
+  else
+    if window_manager == "hyprland"
+    then []
+    else [ ./x11.nix ];
+
 in
+# Pass everything directly into the function, including imports
 functions.makeModuleConfig {
   options = window_manager_options;
   current = window_manager;
   module_name = "window_manager";
-} // {
-    imports = if (builtins.isList window_manager && !(builtins.elem "hyprland" window_manager)) || (!builtins.isList window_manager && window_manager != "hyprland")
-      then [ ./x11.nix ]
-      else [];
+  conditional_imports = conditional_imports;
 }
